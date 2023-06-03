da31c474b073307303fb2bb906b28a6c4e8a14ca
Bullets-jyr <jyr@bullets.co.kr>
2023-06-03 20:40:41 +0900

    04. 계산 기능 구현하기 (2)

--- app/src/main/java/kr/co/bullets/part1chapter5/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part1chapter5/MainActivity.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kr.co.bullets.part1chapter5.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val firstNumberText = StringBuilder("")
     private val secondNumberText = StringBuilder("")
     private val operatorText = StringBuilder("")
+    private val decimalFormat = DecimalFormat("#,###")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,12 +43,12 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val firstNumber = firstNumberText.toString().toInt()
-        val secondNumber = secondNumberText.toString().toInt()
+        val firstNumber = firstNumberText.toString().toBigDecimal()
+        val secondNumber = secondNumberText.toString().toBigDecimal()
 
         val result = when (operatorText.toString()) {
-            "+" -> firstNumber.plus(secondNumber)
-            "-" -> firstNumber - secondNumber
+            "+" -> decimalFormat.format(firstNumber.plus(secondNumber))
+            "-" -> decimalFormat.format(firstNumber - secondNumber)
             else -> "잘못된 수식 입니다."
         }.toString()
 
@@ -71,6 +73,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateEqudationTextView() {
-        binding.equationTextView.text = "$firstNumberText $operatorText $secondNumberText"
+        val firstFormattedNumber = if (firstNumberText.isNotEmpty()) decimalFormat.format(firstNumberText.toString().toBigDecimal()) else ""
+        val secondFormattedNumber = if (secondNumberText.isNotEmpty()) decimalFormat.format(secondNumberText.toString().toBigDecimal()) else ""
+
+        binding.equationTextView.text = "$firstFormattedNumber $operatorText $secondFormattedNumber"
     }
 }
